6346936b2b85744cb1c5cfb2f60eccc883f93b59
Garrett <g@floft.net>
2011-11-30 21:04:29 -0800

    added "New password: " prefix for password.sh

--- install/password.sh
+++ install/password.sh
@@ -4,5 +4,6 @@ file="$dir/.password"
 
 [[ ! -e $dir/design.php ]] && echo "Wrong directory." && exit 1
 
+echo -n "New password: "
 read -s password
 php -r '$p=file_get_contents("php://stdin");echo hash("sha512",$p);' <<< "$password" > "$file"
